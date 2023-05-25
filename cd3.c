ea5c6f820a5e87ff1de24601643bf086966e9c58
ZackTleha <itsmezacc@gmail.com>
2023-05-24 18:08:01 -0700

    more

---
+++ cd3.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/** by ZACKTLEHA AND YHaouane */
+
+/**
+ * update_PWD - updates PWD environment variable
+ * @env: environment variables lists
+ *
+ * Return: 1 (Success) | -1 (Failure)
+ */
+
+int	 update_PWD(char **env)
+{
+	char	 cwd[100], *dir;
+	size_t	 i;
+
+	i = 100, getcwd(cwd, i);
+	for (i = 0; env[i]; i++)
+		if (same(env[i], "PWD"))
+			break;
+	if (!env[i])
+		return (-1);
+	dir = change_var(cwd, env, i);
+	free(env[i]);
+	env[i] = dir;
+	return (1);
+}
+
+/**
+ * update_OLDPWD - updates OLDPWD environment variable
+ * @env: environment variables list
+ * @val: value to update to
+ *
+ * Return: 1 (Success) | -1 (Failure)
+ */
+
+int	 update_OLDPWD(char **env, char *val)
+{
+	char *dir;
+	unsigned int i;
+
+	for (i = 0; env[i]; i++)
+		if (same(env[i], "OLDPWD"))
+			break;
+	if (!env[i])
+		return (-1);
+	dir = change_var(val, env, i);
+	free(env[i]);
+	env[i] = dir;
+	return (1);
+}
